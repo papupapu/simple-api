ea2a07ee724ea1be68c58c657b9d0fddc30fba22
Giancarlo Gerbaz <ganesa_2001@yahoo.it>
2019-12-13 00:55:37 +0100

    bla

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.set('port', (process.env.app_port || 8888));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) { // Additional middleware which will set headers that we need on each request. 
+		console.log(req.url)
     res.setHeader('Access-Control-Allow-Origin', '*'); // Set permissive CORS header - this allows this server to be used only as an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Cache-Control', 'no-cache'); // Disable caching so we'll always get the latest comments.
     next();
